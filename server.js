72926e07904f6088cb0eea19575dae243ee7b64d
vpvnguyen <vpvnguyen@gmail.com>
2020-08-07 19:39:47 -0700

    handle multiple files

--- server.js
+++ server.js
@@ -46,4 +46,18 @@ app.post(
   })
 );
 
+// Uploading Multiple Files
+// Uploading multiple files with Multer is similar
+// to a single file upload, but with a few changes.
+app.post("/uploadMulti", upload.array("myFiles", 12), (req, res, next) => {
+  const { files } = req;
+  if (!files) {
+    const error = new Error("Please choose files");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+
+  res.send(files);
+});
+
 app.listen(PORT, () => console.log(PORT));
