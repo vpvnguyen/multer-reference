df8d874de15775313e4387b7d0cedd07da50377a
Vincent Nguyen <vpvnguyen@gmail.com>
2020-08-08 18:53:24 -0700

    upload middleware reject

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
+const fs = require("fs");
 const { urlencoded } = require("body-parser");
 
 const PORT = 5000;
-
-// mongodb
-const { MongoClient } = require("mongodb");
-const mongoDbUrl = "mongodb://localhost:27017";
 
 // express middleware
 app.use(express.json());
@@ -22,6 +19,8 @@ app.use(urlencoded({ extended: true }));
 // the files a new identifier.
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
+    const dir = "./uploads";
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
     cb(null, "uploads");
   },
   filename: (req, file, cb) => {
@@ -29,7 +28,24 @@ var storage = multer.diskStorage({
   },
 });
 
-var upload = multer({ storage: storage });
+var upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    // The function should call `cb` with a boolean
+    // to indicate if the file should be accepted
+    const acceptedFiles = ["application/pdf"];
+    console.log("FILE FILTER FILE", file);
+    console.log(file);
+
+    // accept file
+    if (acceptedFiles.includes(file.mimetype)) cb(null, true);
+    // reject file
+    cb(null, false);
+
+    // // pass an error if something goes wrong:
+    // cb(new Error("I don't have a clue!"));
+  },
+});
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
@@ -40,15 +56,17 @@ app.get("/", (req, res) => {
 // In the index.html file, we defined an action attribute
 // that performs a POST request. Now we need to create an
 // endpoint in the Express application. Open the server.js file
-app.post("/uploadFile", upload.single("myFile"), (req, res, next) => {
+app.post("/uploadFile", upload.single("myFile"), async (req, res, next) => {
   const { file } = req;
-  console.log(file);
-  if (!file) {
-    const error = new Error("Please upload a file");
-    error.httpStatusCode = 400;
-    return next(error);
-  }
-  res.send(file);
+  console.log("DO SOMETHING ELSE");
+  if (upload.single("myFile")) console.log(upload.single("myFile"));
+  // if (!file) {
+  //   const error = new Error("Please upload a file");
+  //   error.httpStatusCode = 400;
+  //   return next(error);
+  // }
+
+  res.send({ createIssue: "success" });
 });
 
 // Uploading Multiple Files
@@ -65,8 +83,4 @@ app.post("/uploadMulti", upload.array("myFiles", 12), (req, res, next) => {
   res.send(files);
 });
 
-MongoClient.connect(mongoDbUrl, { useUnifiedTopology: true }, (err, client) => {
-  if (err) return console.error(err);
-  db = client.db("test");
-  app.listen(PORT, () => console.log(`App running on ${PORT}`));
-});
+app.listen(PORT, () => console.log(`App running on ${PORT}`));
